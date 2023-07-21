73693ba3fa3304b0a0f3f112dcc909d68b7ac8e0
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:18 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/SpeedMod.java
+++ src/main/java/com/peasenet/mod/SpeedMod.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SpeedMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
         GavinsMod.addMod(new ModSpeed());
     }
 }
